fe3a40e3c25441e22c39262044f459ef26368722
Pradixx <dieguittosilva09@gmail.com>
2025-12-08 14:08:44 -0300

    Usuario repository, mudança em todas as chamadas

--- src/main/java/com/deigo/apiTarefas/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/deigo/apiTarefas/infrastructure/repository/UsuarioRepository.java
@@ -4,12 +4,11 @@ import com.deigo.apiTarefas.infrastructure.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findById(UUID id);
 
@@ -18,6 +17,4 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     void deleteById(UUID id);
 
     Optional<Object> findByEmail(String email);
-
-    List<Tarefa> findByUsuarioId(Long usuarioId);
 }
