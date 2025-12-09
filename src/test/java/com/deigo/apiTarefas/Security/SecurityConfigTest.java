1ac2961f4c4f86f4aedb1b0e09b96806faa5a53d
Pradixx <dieguittosilva09@gmail.com>
2025-12-09 16:57:03 -0300

    Security config Test

---
+++ src/test/java/com/deigo/apiTarefas/Security/SecurityConfigTest.java
@@ -0,0 +1,20 @@
+package com.deigo.apiTarefas.Security;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+@Profile("test")
+public class SecurityConfigTest {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
